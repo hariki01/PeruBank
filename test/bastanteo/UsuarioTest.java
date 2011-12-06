bf9bf46a9e3d47a8c424f8c4c801b6e9446c7a0b
jorvil <jorvil_06@hotmail.com>
2011-12-05 21:23:17 -0500

    Subido por Jorge Alcntara

--- test/bastanteo/UsuarioTest.java
+++ test/bastanteo/UsuarioTest.java
@@ -9,12 +9,14 @@ public class UsuarioTest {
 	@Test
 	public void usuarioAdd() throws ClienteException{
 		// Administrador - Jorge Alcantara
+		
 		// Carga Inicial 1
 		// Carga Inicial 2
 		// Carga Inicial 3
 		// Carga Inicial 4
 		// Carga Inicial 5
 		// Carga Inicial 6
+		// Carga Inicial 7
 		String nombre = "HANS ESPIRITU";
 		String documento = "12345678";
 		String email = "HANS@HOTMAIL.COM";
